1f29e767a0fa94b9e301a1d73fbb8547c733d5e1
Gabriel dos Santos <gabriel.dsantos@unidas.com.br>
2025-03-12 17:14:02 -0300

    ajuste git

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,14 @@
 import axios from 'axios';
 import { QueueResponse, PayloadResponse } from './types';
 
+// Create a proxy URL based on the environment
+const BASE_URL = import.meta.env.PROD 
+  ? 'https://unidas.my.salesforce.com'
+  : '/api';
+
 export async function fetchQueueData(quoteId: string, token: string) {
   const response = await axios.get<QueueResponse>(
-    `/api/services/data/v57.0/query/?q=SELECT+FIELDS(ALL)+FROM+Queue__c+WHERE+keyField__c='${quoteId}'+limit+100`,
+    `${BASE_URL}/services/data/v57.0/query/?q=SELECT+FIELDS(ALL)+FROM+Queue__c+WHERE+keyField__c='${quoteId}'+limit+100`,
     {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -16,7 +21,7 @@ export async function fetchQueueData(quoteId: string, token: string) {
 
 export async function fetchPayloadData(queueId: string, token: string) {
   const response = await axios.get<PayloadResponse>(
-    `/api/services/data/v57.0/query/?q=SELECT+FIELDS(ALL)+FROM+Payload_Handler__c+WHERE+Queue__c='${queueId}'+limit+100`,
+    `${BASE_URL}/services/data/v57.0/query/?q=SELECT+FIELDS(ALL)+FROM+Payload_Handler__c+WHERE+Queue__c='${queueId}'+limit+100`,
     {
       headers: {
         Authorization: `Bearer ${token}`,
